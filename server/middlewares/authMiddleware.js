72661a9f623c4d5149ae144bf0cc1bb804baf687
Sujeet Sharma <sujeetsharmadc56@gmail.com>
2026-05-11 01:14:07 +0530

    ignore env and update frontend

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken'
 
 const protect = async (req, res, next) => {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
     if(!token){
         return res.status(401).json({ message: 'Unauthorized' });
     }
